8f64413ef42716045b4491b4cbc3589009ba5ff9
Konrad <nosiuodkrywca@gmail.com>
2023-04-21 23:12:56 +0200

    added esp8266 voltage monitor

---
+++ esp8266/voltage_monitor.ino
@@ -0,0 +1,149 @@
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <ESP8266WebServer.h>
+#include <Wire.h>
+
+#define sgn(x) ((x) < 0 ? -1 : ((X) > 0 ? 1 : 0))
+
+WiFiManager wm;
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+unsigned long currT = 0;
+unsigned long prevT = 0;
+unsigned long diffT = 0;
+
+// this is the integer offset on raw sensor data, eg. background noise
+int zeroOffset = -1;
+
+// this is a static voltage offset value applied to the final result
+float voltOffset = -0.05;
+
+// precision of the sensor, 1024 for 10-bit
+int precision = 1024;
+
+// accepted maximum voltage of the board
+float inputRange = 3.2;
+
+// resistance of the first resistor (Vin)
+int resI = 4700;
+
+// resistance of the second resistor (GND)
+int resO = 330;
+
+// voltage divider ratio
+float divRatio = (resI + resO) / resO;
+
+// capacity of the battery bank (in Ah)
+int capacity = 320;
+
+// voltage of a fully charged battery
+float fullyCharged = 12.9;
+
+// voltage of an empty battery
+float totallyEmpty = 10.8;
+
+// voltage drop of battery bank under 1c load (in %)
+int cdrop = 4;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  //wm.resetSettings();                     // uncomment to reset network settings
+  wm.autoConnect();
+
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+
+}
+
+
+
+void loop(void){
+  server.handleClient();                    // Listen for HTTP requests from clients
+}
+
+void handleRoot() {
+  float voltage = measure();
+  server.send(200, "text/plain", String(voltage));   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+float measure() {
+
+  float res[10] = {};
+  int sensorValue;
+  float voltage;
+
+  int i = 0;
+
+  while(i < 10) {
+
+    currT = millis();
+    diffT = currT - prevT;
+
+    if (diffT>20) {
+      Serial.print("measurement no #");
+      Serial.print(i);
+      Serial.print(": ");
+      
+      sensorValue = analogRead(A0);
+      voltage = (sensorValue + zeroOffset) * (1.0/precision) * divRatio * inputRange + voltOffset;
+      
+      res[i] = voltage;
+      Serial.print(voltage);
+      Serial.println(" V");
+
+      prevT = currT;      
+      i += 1;
+    }
+
+
+  }
+
+  return mode(res);
+  
+}
+
+float mode(float arr[]) {
+
+  //int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    int n = 10;
+    /*Here we take two parameters, the beginning of the
+    array and the length n upto which we want the array to
+    be sorted*/
+    std::sort(arr, arr + n);
+
+  //int[10] sortedArray; // this array must be sorted!!
+
+  int modeCt = 0;
+  float modeV = -1;
+
+  int ct = 0;
+  float v = -1.;
+
+  for(int i = 0; i< sizeof(arr); i++) {
+    if(arr[i] != v) {
+      v = arr[i];
+      ct = 0;
+    }
+
+    ct ++;
+
+    if(ct > modeCt) {
+      modeCt = ct;
+      modeV = v;
+    }
+  }
+  Serial.print("mode: ");
+  Serial.println(modeV);
+  return modeV;
+}
